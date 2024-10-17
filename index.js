2b4669182f3ba020aada0bc13fbf4d23d1a1f52e
Rohan Acharya <rohancharya42@gmail.com>
2024-10-17 20:36:22 +0545

    Add the logic to handle APIHit and process the incomming data

--- index.js
+++ index.js
@@ -1,2 +1,26 @@
-const changeThis = document.querySelector("h1");
-changeThis.style.color = "red";
+const userInput = document.querySelector("#userInput");
+
+userInput.addEventListener("submit", async function (event) {
+  event.preventDefault();
+
+  const location = document.getElementById("location").value;
+
+  try {
+    const currentConditions = await hitAPI(location);
+    console.log(currentConditions.currentConditions);
+  } catch (error) {
+    console.log("Error(from eventlistner function): ", error);
+  }
+});
+
+async function hitAPI(location) {
+  try {
+    const data = await fetch(
+      `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=5C2K3RFNADS7XS3Q24829L8HR`
+    );
+    const response = await data.json();
+    return response;
+  } catch (error) {
+    console.log("Error: ", error);
+  }
+}
