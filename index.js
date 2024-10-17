9907fd7611115730c82d8c7eac2d105c29e4cb63
Rohan Acharya <rohancharya42@gmail.com>
2024-10-17 21:03:22 +0545

    Add Gipsy to showcase current weather condition with it's API Integration

--- index.js
+++ index.js
@@ -4,15 +4,37 @@ userInput.addEventListener("submit", async function (event) {
   event.preventDefault();
 
   const location = document.getElementById("location").value;
+  const img = document.querySelector(".weatherGipsy");
 
+  //To handle the weather API value
   try {
-    const currentConditions = await hitAPI(location);
-    console.log(currentConditions.currentConditions);
+    const loggedData = await hitAPI(location);
+    console.log(loggedData.currentConditions);
+    const condition = loggedData.currentConditions.conditions;
+    weatherGipsy(condition);
   } catch (error) {
     console.log("Error(from eventlistner function): ", error);
   }
+
+  //To Handle the appropirate Gipsy for the current weather condition
+  async function weatherGipsy(condition) {
+    try {
+      const response = await fetch(`https://api.giphy.com/v1/gifs/translate?api_key=3Nnbnu5G5xbCIE1Cnfs3H3BI7wpjnlka&s=${condition} weather`);
+
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+      const data = await response.json();
+
+      const url = data.data.images.original.url;
+      img.src = url;
+    } catch (error) {
+      errorMsg.textContent = `Fetch error: ${error.message}`;
+    }
+  }
 });
 
+// To handle Weather APIHIT
 async function hitAPI(location) {
   try {
     const data = await fetch(
