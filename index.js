247fb25d50ff52afb66d9fd3bc04fe56415a53ee
Rohan Acharya <rohancharya42@gmail.com>
2024-10-17 21:45:24 +0545

    Add logic to handle display of the collected data

--- index.js
+++ index.js
@@ -1,25 +1,29 @@
 const userInput = document.querySelector("#userInput");
+const img = document.querySelector(".weatherGipsy");
+
+img.style.display = "none";
 
 userInput.addEventListener("submit", async function (event) {
   event.preventDefault();
 
   const location = document.getElementById("location").value;
-  const img = document.querySelector(".weatherGipsy");
 
   //To handle the weather API value
   try {
     const loggedData = await hitAPI(location);
-    console.log(loggedData.currentConditions);
-    const condition = loggedData.currentConditions.conditions;
+    const liveResult = loggedData;
+    const condition = liveResult.currentConditions.conditions;
     weatherGipsy(condition);
+    handleDisplay(liveResult);
   } catch (error) {
     console.log("Error(from eventlistner function): ", error);
   }
 
   //To Handle the appropirate Gipsy for the current weather condition
   async function weatherGipsy(condition) {
+    img.style.display = "block";
     try {
-      const response = await fetch(`https://api.giphy.com/v1/gifs/translate?api_key=3Nnbnu5G5xbCIE1Cnfs3H3BI7wpjnlka&s=${condition} weather`);
+      const response = await fetch(`https://api.giphy.com/v1/gifs/translate?api_key=3Nnbnu5G5xbCIE1Cnfs3H3BI7wpjnlka&s=${condition} weather sky`);
 
       if (!response.ok) {
         throw new Error(`HTTP error! Status: ${response.status}`);
@@ -45,4 +49,16 @@ async function hitAPI(location) {
   } catch (error) {
     console.log("Error: ", error);
   }
+}
+
+function handleDisplay(liveResult) {
+  const temperature = document.querySelector(".temp");
+  const location = document.querySelector(".loc");
+  const description = document.querySelector(".des");
+
+  temperature.textContent = `${Math.round(liveResult.currentConditions.temp)}°F | ${Math.round(
+    ((liveResult.currentConditions.temp - 32) * 5) / 9
+  )}°C`;
+  location.textContent = liveResult.resolvedAddress;
+  description.textContent = liveResult.description;
 }
